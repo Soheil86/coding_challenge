8c9e687f8b394868980780a74b2d1cafa661b7a6
Soheil86 <soheilghanbari1365@gmail.com>
2022-09-21 16:43:56 +0200

    initial commit

--- src/Components/Startup/StartupList.tsx
+++ src/Components/Startup/StartupList.tsx
@@ -1,5 +1,34 @@
 import { Fragment, ReactElement } from "react";
+import { StartupDTO } from "../../Types/Startup";
 
-export default function StartupList(): ReactElement {
-  return <Fragment></Fragment>;
-}
+
+// const StartupList: React.FC<StartupDTO> = ({ response }) => {
+export default function StartupList ()  {
+  return (
+    <div>StartUp</div>
+  )
+   }
+  
+ 
+  // return response.map((startup) => {
+    
+      
+  //   <Card>
+          
+  //         <h1>{startup.name}</h1>
+       
+  //         <div>
+  //         <h2>{startup.dateFounded}</h2>
+  //         <h2>{startup.employees}</h2>
+  //         <h2>{startup.totalFunding}</h2>
+  //         <h2>{startup.currentInvestmentStage}</h2>
+  //         </div>
+  //       <div>
+  //           <h2>{startup.shortDescription}</h2>
+  //       </div>
+  //    </Card>
+  //   )
+  // })
+
+
+// export default StartupList
--- src/Http/Startup/Startup.http.service.ts
+++ src/Http/Startup/Startup.http.service.ts
@@ -1,10 +1,15 @@
-import axios from "axios";
-import { Startup, StartupDTO } from "../../Types/Startup";
-import StartupMapper from "./Startup.mapper";
+import axios from 'axios'
+import { Startup, StartupDTO } from '../../Types/Startup'
+import StartupMapper from './Startup.mapper'
+import StartupList from '../../Components/Startup/StartupList'
 
 export class StartupHttpService {
   public static async getStartupById(id: string | number): Promise<Startup> {
-    const response = await axios.get<StartupDTO>(`/api/startups/${id}`);
-    return StartupMapper.map(response.data);
+    const response = await axios.get<StartupDTO>(`/api/startups/${id}`)
+    return StartupMapper.map(response.data)
+    // StartupMapper.map(item => (
+    //   // < StartupList response = { response } />
+    // ))
+    // < StartupList response = { response } />
   }
 }
